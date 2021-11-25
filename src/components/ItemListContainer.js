80e710f6a8e86d32c5ef8c97ce346996f6248c4a
Camila Rojas <5bxsuzva@gmail.com>
2021-11-25 00:01:45 -0300

    se agrego un greeting

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,12 +4,15 @@ const ItemListContainer = ({greeting}) => {
 
     const style = {
         backgroundColor: 'yellow',
-        height: 200
+        height: 200,
+        padding: 40
     }
 
     return (
-        <h1 style={style}></h1>
+        <h1 style={style}>{greeting}</h1>
     )
 }
+
+
 
 export default ItemListContainer;
